f7ba46ae656548261edcb5990e2c87ee413b50a7
Jon Anwyl <jon.anwyl@gmail.com>
2021-01-16 12:15:08 +0000

    Move SBI changes in from child theme

--- templates/item.php
+++ templates/item.php
@@ -11,48 +11,33 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 }
-$classes = SB_Instagram_Display_Elements::get_item_classes( $settings, $offset );
-$post_id = SB_Instagram_Parse::get_post_id( $post );
-$timestamp = SB_Instagram_Parse::get_timestamp( $post );
-$media_type = SB_Instagram_Parse::get_media_type( $post );
-$permalink = SB_Instagram_Parse::get_permalink( $post );
-$maybe_carousel_icon = $media_type === 'carousel' ? SB_Instagram_Display_Elements::get_icon( 'carousel', $icon_type ) : '';
-$maybe_video_icon = $media_type === 'video' ? SB_Instagram_Display_Elements::get_icon( 'video', $icon_type ) : '';
-$media_url = SB_Instagram_Display_Elements::get_optimum_media_url( $post, $settings, $resized_images );
-$media_full_res = SB_Instagram_Parse::get_media_url( $post );
-$sbi_photo_style_element = SB_Instagram_Display_Elements::get_sbi_photo_style_element( $post, $settings ); // has already been escaped
-$media_all_sizes_json = SB_Instagram_Parse::get_media_src_set( $post, $resized_images );
 
-/**
- * Text that appears in the "alt" attribute for this image
- *
- * @param string $img_alt full caption for post
- * @param array $post api data for the post
- *
- * @since 2.1.5
- */
-$img_alt = SB_Instagram_Parse::get_caption( $post, sprintf( __( 'Instagram post %s', 'instagram-feed' ), $post_id ) );
-$img_alt = apply_filters( 'sbi_img_alt', $img_alt, $post );
-
-/**
- * Text that appears in the visually hidden screen reader element
- *
- * @param string $img_screenreader first 50 characters for post
- * @param array $post api data for the post
- *
- * @since 2.1.5
- */
-$img_screenreader = substr( SB_Instagram_Parse::get_caption( $post, sprintf( __( 'Instagram post %s', 'instagram-feed' ), $post_id ) ), 0, 50 );
-$img_screenreader = apply_filters( 'sbi_img_screenreader', $img_screenreader, $post );
+$item = st_sbi_parse_item( $post, $settings, $offset, $icon_type, $resized_images );
 
 ?>
-<div class="sbi_item sbi_type_<?php echo esc_attr( $media_type ); ?><?php echo esc_attr( $classes ); ?>" id="sbi_<?php echo esc_html( $post_id ); ?>" data-date="<?php echo esc_html( $timestamp ); ?>">
-    <div class="sbi_photo_wrap">
-        <a class="sbi_photo" href="<?php echo esc_url( $permalink ); ?>" target="_blank" rel="noopener nofollow" data-full-res="<?php echo esc_url( $media_full_res ); ?>" data-img-src-set="<?php echo esc_attr( sbi_json_encode( $media_all_sizes_json ) ); ?>"<?php echo $sbi_photo_style_element; ?>>
-            <span class="sbi-screenreader"><?php echo esc_html( $img_screenreader ); ?></span>
-            <?php echo $maybe_carousel_icon; ?>
-	        <?php echo $maybe_video_icon; ?>
-            <img src="<?php echo esc_url( $media_url ); ?>" alt="<?php echo esc_attr( $img_alt ); ?>">
-        </a>
-    </div>
+<div class="sbi_item sbi_type_<?php echo esc_attr( $item->media_type ); ?><?php echo esc_attr( $item->classes ); ?>" id="sbi_<?php echo esc_html( $item->post_id ); ?>" data-date="<?php echo esc_html( $item->timestamp ); ?>">
+    <?php if ( 'carousel' == $item->media_type ) {
+        if ( isset( $post['children']['data'] ) && is_array( $post['children']['data'] ) ) {
+            echo '<div class="sbi_photo_wrap">';
+            foreach ( $post['children']['data'] as $key => $child ) {
+                $display = 0 === $key ? true : false;
+                $child_item = st_sbi_parse_item( $child, $settings, $offset, $icon_type, $resized_images );
+                if ( 'video' == $child_item->media_type ) {
+                    st_sbi_render_video_html( $child_item, $display );
+                } else {
+                    $icon_html = $display ? SB_Instagram_Display_Elements::get_icon( 'carousel', $item->icon_type ) : '';
+                    st_sbi_render_image_html( $child_item, $display, $icon_html );
+                }
+            }
+            echo '</div>';
+        }
+    } elseif ( 'video' == $item->media_type ) {
+        echo '<div class="sbi_photo_wrap">';
+        st_sbi_render_video_html( $item );
+        echo '</div>';
+    } else {
+        st_sbi_render_image_html( $item );
+    }
+    ?>
+    <div class="sbi-caption"><?php esc_html_e( $item->caption ); ?></div>
 </div>
